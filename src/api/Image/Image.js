9a1542322a92df892a9b50ae048495cd0657f40d
Charles Raimbault <charles.raimbault@epitech.eu>
2020-09-11 19:53:36 +0200

    [ADD] #Image - ImageBase

---
+++ src/api/Image/Image.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const ImageBase = require("./ImageBase");
+
+module.exports = {
+	ImageBase,
+};
---
+++ src/api/Image/ImageBase.js
@@ -0,0 +1,21 @@
+"use scrict";
+const rp = require("request-promise");
+
+module.exports = async function (imageHash) {
+	const options = {
+		method: "GET",
+		uri: `https://api.imgur.com/3/image/${imageHash}`,
+		headers: {
+			Authorization: `Client-ID ${this.clientId}`,
+		},
+		json: true,
+	};
+
+	return rp(options)
+		.then(function (response) {
+			return response.data;
+		})
+		.catch(function (err) {
+			return err;
+		});
+};
