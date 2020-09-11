996f81b8fa56cab023e4e934147088f815b834c4
Charles Raimbault <charles.raimbault@epitech.eu>
2020-09-11 19:54:36 +0200

    [ADD] prototype methodes

--- src/client.js
+++ src/client.js
@@ -1,4 +1,7 @@
 const Account = require("./api/Account/Account");
+const Album = require("./api/Album/Album");
+const Comment = require("./api/Comment/Comment");
+const Image = require("./api/Image/Image");
 
 class ImgurClient {
 	constructor(loginParams) {
@@ -13,5 +16,15 @@ ImgurClient.prototype.isAccountBlocked = Account.AccountBlockStatus;
 ImgurClient.prototype.getAccountImages = Account.AccountImages;
 ImgurClient.prototype.getAccountFavorites = Account.AccountFavorites;
 
-//
+// Album functions
+ImgurClient.prototype.getAlbumInformations = Album.AlbumBase;
+ImgurClient.prototype.getAlbumImages = Album.AlbumImages;
+
+// Comment functions
+ImgurClient.prototype.getCommentInformations = Comment.CommentBase;
+ImgurClient.prototype.getCommentReplies = Comment.Replies;
+
+// Image Function
+ImgurClient.prototype.getImageInformations = Image.ImageBase;
+
 module.exports = ImgurClient;
