0ade0df12c36510f196e4e128fe7938f70084653
Charles Raimbault <charles.raimbault@epitech.eu>
2020-09-11 03:24:42 +0200

    [DELETE] test folder

--- test/index.js
+++
@@ -1,6 +0,0 @@
-require("dotenv").config();
-const ImgurClient = require("../index");
-const client = new ImgurClient({clientId: process.env.CLIENT_ID});
-// client.getAccountInformations("username").then(console.log);
-// client.isAccountBlocked("username").then(console.log);
-client.getAccountFavorites("charlesra").then(console.log);
