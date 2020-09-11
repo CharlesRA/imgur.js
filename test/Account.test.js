f68c10d55ae2407212588f93efaff20cc1a3a812
Charles Raimbault <charles.raimbault@epitech.eu>
2020-09-12 01:35:13 +0200

    [ADD] more tests

--- test/Account.test.js
+++ test/Account.test.js
@@ -1,19 +1,37 @@
 require("dotenv").config();
 const ImgurClient = require("../index");
-const client = new ImgurClient({clientId: process.env.CLIENT_ID});
+const unauthClient = new ImgurClient({clientId: process.env.CLIENT_ID});
+const authClient = new ImgurClient({clientId: process.env.CLIENT_ID, accessToken: process.env.ACCESS_TOKEN});
 const expect = require("chai").expect;
 
 describe("Account", function () {
+	this.timeout(10000);
 	it("isAccountBlocked", async function () {
-		const result = await client.isAccountBlocked("charlesra");
+		const result = await unauthClient.isAccountBlocked("charlesra");
 		expect(result).to.be.equal(false);
 	});
+	it("getAccountAvatar", async function () {
+		const result = await authClient.getAccountAvatar("charlesra");
+		expect(typeof result.avatar).to.be.equal("string");
+	});
 	it("getAccountFavorites", async function () {
-		const result = await client.getAccountFavorites("charlesra");
+		const result = await unauthClient.getAccountFavorites("charlesra");
 		expect(result.length > 0).to.be.equal(true);
 	});
 	it("getAccountInformations", async function () {
-		const result = await client.getAccountInformations("charlesra");
+		const result = await unauthClient.getAccountInformations("charlesra");
 		expect(result.id).to.be.equal(114360911);
+	});
+	it("getAccountImages", async function () {
+		const result = await authClient.getAccountImages("charlesra");
+		expect(result.length > 0).to.be.equal(true);
+	});
+	it("getAccountReplies", async function () {
+		const result = await authClient.getAccountReplies();
+		expect(result).to.not.be.equal(undefined);
+	});
+	it("getAccountSettings", async function () {
+		const result = await authClient.getAccountSettings();
+		expect(result.account_url).to.be.equal("CharlesRA");
 	});
 });
--- test/Image.test.js
+++ test/Image.test.js
@@ -1,11 +1,31 @@
 require("dotenv").config();
 const ImgurClient = require("../index");
-const client = new ImgurClient({clientId: process.env.CLIENT_ID});
+const unauthClient = new ImgurClient({clientId: process.env.CLIENT_ID});
+const authClient = new ImgurClient({clientId: process.env.CLIENT_ID, accessToken: process.env.ACCESS_TOKEN});
 const expect = require("chai").expect;
 
 describe("Image", function () {
+	this.timeout(10000);
+	let id = undefined;
 	it("getImageInformations", async function () {
-		const result = await client.getImageInformations("jXQHyU4");
+		const result = await unauthClient.getImageInformations("jXQHyU4");
 		expect(result.id).to.be.equal("jXQHyU4");
+	});
+	it("changeStatusFavoriteImage", async function () {
+		const result = await authClient.changeStatusFavoriteImage("jXQHyU4");
+		expect(result === "favorited" || result === "unfavorited").to.be.equal(true);
+	});
+	it("upload", async function () {
+		const result = await authClient.upload({image: "https://upload.wikimedia.org/wikipedia/commons/3/37/African_Bush_Elephant.jpg"}, false);
+		id = result.id;
+		expect(typeof result.id).to.be.equal("string");
+	});
+	it("updateImageInformation", async function () {
+		const result = await authClient.updateImageInformation(id, {title: "Title change", description: "Description change"});
+		expect(result).to.be.equal(true);
+	});
+	it("remove", async function () {
+		const result = await authClient.remove(id);
+		expect(result).to.be.equal(true);
 	});
 });
