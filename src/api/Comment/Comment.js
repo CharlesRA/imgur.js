c2ae1578fde5e8dfe733e523617c8ae871658406
Charles Raimbault <charles.raimbault@epitech.eu>
2020-09-14 02:15:35 +0200

    [UPDATE] handle errors

--- src/api/Comment/Comment.js
+++ src/api/Comment/Comment.js
@@ -2,8 +2,14 @@
 
 const CommentBase = require("./CommentBase");
 const Replies = require("./Replies");
+const Vote = require("./Vote");
+const CommentCreation = require("./CommentCreation");
+const CommentDeletion = require("./CommentDeletion");
 
 module.exports = {
 	CommentBase,
 	Replies,
+	Vote,
+	CommentCreation,
+	CommentDeletion,
 };
