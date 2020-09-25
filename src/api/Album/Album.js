e88964623bac867444c5f5f279f6757e7ad765d3
Charles Raimbault <charles.raimbault@epitech.eu>
2020-09-25 20:03:50 +0200

    format

--- src/api/Album/Album.js
+++ src/api/Album/Album.js
@@ -1,7 +1,9 @@
 const AlbumBase = require("./AlbumBase");
 const AlbumImages = require("./AlbumImages");
+const FavoriteAlbum = require("./FavoriteAlbum");
 
 module.exports = {
 	AlbumBase,
 	AlbumImages,
+	FavoriteAlbum,
 };
--- test/Album.test.js
+++ test/Album.test.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const ImgurClient = require("../index");
-const client = new ImgurClient({clientId: process.env.CLIENT_ID});
+const client = new ImgurClient({
+	clientId: process.env.CLIENT_ID,
+	accessToken: process.env.ACCESS_TOKEN,
+});
 const expect = require("chai").expect;
 
 describe("Album", function () {
@@ -11,5 +14,11 @@ describe("Album", function () {
 	it("getAlbumImages", async function () {
 		const result = await client.getAlbumImages("U2IKcAG");
 		expect(result.length).to.be.equal(6);
+	});
+	it("changeAlbumFavoriteStatus", async function () {
+		const result = await client.changeAlbumFavoriteStatus("naOyG");
+		expect(result == "favorited" || result == "unfavorited").to.be.equal(
+			true,
+		);
 	});
 });
